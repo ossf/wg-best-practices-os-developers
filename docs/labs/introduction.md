9940670785317b52e3da708466d13e44fc58d5be
David A. Wheeler <dwheeler@dwheeler.com>
2024-03-12 14:37:42 -0400

    Minor tweaks per tool review
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- docs/labs/introduction.md
+++ docs/labs/introduction.md
@@ -26,3 +26,10 @@ a widely-used server-side framework for JavaScript, along with
 However, we'll often fill in other parts, so you can focus on the
 specific issues we're discussing.
 
+Many labs compare your attempt against a pattern for an expected
+correct answer. In many cases you can find an obscure way to write
+code that would produce the correct result yet not counted as a correct
+answer. For example, instead of writing `1` you could write `(2-1)` or
+`0x1`. We are looking for code that is *simple* and *clear*, not just
+code that would produce the correct answer.
+
--- docs/labs/lab_checker.js
+++ docs/labs/lab_checker.js
@@ -3,12 +3,14 @@
 // Correct answers are embedded in the web page in a div area with
 // the id "correct".
 // Answers are expressed using regular expression patterns, to make it easy to
-// indicate the many different forms that are all correct.
-// Pattern `(a|b)` matches either `a` or `b`, while `foo\(a\)` matches `foo(a)`.
-// Pattern `\{\\\}` matches the literal text `{\}`.
+// indicate the many different forms that are all correct. E.g.:
+//
+// * Pattern `(a|b)` matches `a` or `b`, while `foo\(a\)` matches `foo(a)`.
+// * Pattern `\{\\\}` matches the literal text `{\}`.
+// * Pattern `9_?999` matches `9`, an optional `_`, then `999`.
 //
 // To make the correct answer regular expressions easier to read, the
-// correct pattern is preprocessed as follows:
+// pattern for correct answers is preprocessed as follows:
 // * A completely blank line in the middle of a pattern is interpreted as
 //   a required end of line.
 // * Otherwise, any sequence of 1+ whitespace (spaces, tabs, and newlines)
@@ -18,8 +20,16 @@
 // * Use \s to match a whitespace character, and \x20 for a space character.
 // * The given pattern much be exactly matched - it's case-sensitive.
 // * The *entire* input must match the correct answer. Leading and trailing
-//   whitespace in the input is ignored, though. That is,
-// * "^\s*" is added at the beginning and "\s*$" at the end.
+//   newlines are removed. Answers can match with leading and trailing spaces
+//   by default (if your pattern *requires* spaces, then they'll be required).
+
+/**
+ * Trim newlines (LF or CRLF) from beginning and end of given String.
+ */
+function trimNewlines(s) {
+    return ((s + '').replace(/^\r?\n(\r?\n)+/, '')
+            .replace(/^\r?\n(\r?\n)+$/, ''));
+}
 
 /**
  * Return if attempt matches the regex correct.
@@ -30,11 +40,10 @@
  */
 function calcMatch(attempt, correct) {
     try {
-          let unsweetened_re = (
-            '^\s*' +
-           correct.replace(/\r?\n( *\r?\n)+/g,'\\r?\\n').replace(/\s+/g,'\\s*') +
-            '\s*$'
-          );
+          let unsweetened_re = ('^ *' +
+                  (correct.replace(/\r?\n( *\r?\n)+/g,'\\r?\\n')
+                          .replace(/\s+/g,'\\s*')) +
+                  ' *$');
           let re = new RegExp(unsweetened_re);
 
           return (re.test(attempt));
@@ -50,12 +59,13 @@ function calcMatch(attempt, correct) {
  * Then set "grade" in document depending on that answer.
  */
 function run_check() {
-    let attempt = document.getElementById('attempt').value.trim();
+    // Ignore empty lines at beginning & end of both attempt and correct.
+    let attempt = trimNewlines(document.getElementById('attempt').value);
 
     // We could optimize this by creating the regex once per page.
     // However, JavaScript regexes are stateful, so we'd need to be careful,
     // and it's currently so fast that it doesn't matter.
-    let correct = document.getElementById('correct').textContent.trim();
+    let correct = trimNewlines(document.getElementById('correct').textContent);
 
     // Calculate grade and set in document.
     let isCorrect = calcMatch(attempt, correct);
