5f3c234cf66805f6f6a68d5d9fbe73754f26d33b
David A. Wheeler <dwheeler@linuxfoundation.org>
2024-03-12 16:22:10 -0400

    Create initial stub for lab exercises (#370)
    * Create initial stub for lab exercises
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Improvements towards making lab input1 work
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Adjust validator
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Move emulation approach into an "alt" file
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Switch to using a "lab_checker" instead of full emulation
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Clean up lab_checker code
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Change input background color depending on correctness
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Make lab input1's correct answer more flexible
    
    This probably isn't worth it here, but it's a useful example.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Tweak introduction since we're taking a different approach
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Lab input1 - clarify instructions
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Tweak "correct" preprocess for end-of-line matching
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Minor tweaks per tool review
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * lab introduction.md: Fix markdown per markdownlint
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Add some clarifying comments to the JavaScript
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * introduction.md: Improve explanation on why some answers are rejected
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * Clearly note that these aren't approved yet.
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    ---------
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ docs/labs/lab_checker.js
@@ -0,0 +1,92 @@
+// lab_checker - check and report if lab attempt is correct
+
+// TODO: The current display for failing and success is ugly.
+// We could do better.
+
+// Correct answers are embedded in the web page in a div area with
+// the id "correct".
+// Answers are expressed using regular expression patterns, to make it easy to
+// indicate the many different forms that are all correct. E.g.:
+//
+// * Pattern `(a|b)` matches `a` or `b`, while `foo\(a\)` matches `foo(a)`.
+// * Pattern `\{\\\}` matches the literal text `{\}`.
+// * Pattern `9_?999` matches `9`, an optional `_`, then `999`.
+//
+// To make the correct answer regular expressions easier to read, the
+// pattern for correct answers is preprocessed as follows:
+// * A completely blank line in the middle of a pattern is interpreted as
+//   a required end of line.
+// * Otherwise, any sequence of 1+ whitespace (spaces, tabs, and newlines)
+//   is interpreted as "0 or more whitespace is allowed here".
+//   This can also be expressed as `\s*`, but whitespace is easier to read,
+//   and this circumstance repeatedly occurs in correct answers.
+// * Use \s to match a whitespace character, and \x20 for a space character.
+// * The given pattern much be exactly matched - it's case-sensitive.
+// * The *entire* input must match the correct answer. Leading and trailing
+//   newlines are removed. Answers can match with leading and trailing spaces
+//   by default (if your pattern *requires* spaces, then they'll be required).
+
+/**
+ * Trim newlines (LF or CRLF) from beginning and end of given String.
+ */
+function trimNewlines(s) {
+    return ((s + '').replace(/^\r?\n(\r?\n)+/, '')
+            .replace(/^\r?\n(\r?\n)+$/, ''));
+}
+
+/**
+ * Return if attempt matches the regex correct.
+ * @attempt String from user
+ * @correct String of regex describing correct answer
+ *
+ * This shows an alert if "correct" isn't syntactically valid.
+ */
+function calcMatch(attempt, correct) {
+    try {
+          let unsweetened_re = ('^ *' +
+                  (correct.replace(/\r?\n( *\r?\n)+/g,'\\r?\\n')
+                          .replace(/\s+/g,'\\s*')) +
+                  ' *$');
+          let re = new RegExp(unsweetened_re);
+
+          return (re.test(attempt));
+      }
+      catch(e) {
+          alert(`Unparsable correct answer "${correct}"`);
+          return false;
+      }
+}
+
+/**
+ * Check the document's user input "attempt" to see if matches "correct".
+ * Then set "grade" in document depending on that answer.
+ */
+function run_check() {
+    // Ignore empty lines at beginning & end of both attempt and correct.
+    let attempt = trimNewlines(document.getElementById('attempt').value);
+
+    // We could optimize this by creating the regex once per page.
+    // However, JavaScript regexes are stateful, so we'd need to be careful,
+    // and it's currently so fast that it doesn't matter.
+    let correct = trimNewlines(document.getElementById('correct').textContent);
+
+    // Calculate grade and set in document.
+    let isCorrect = calcMatch(attempt, correct);
+    document.getElementById('grade').innerHTML = isCorrect ?
+      '<span class="success">SUCCESS! That is correct.</span>' :
+      '<span class="fail">Sorry, that is not correct.</span>';
+    document.getElementById('attempt').style.backgroundColor = isCorrect ?
+      'lightgreen' : 'yellow';
+
+}
+
+function init_page() {
+    // This will cause us to sometimes check twice, but this also ensures
+    // that we always catch changes to the attempt.
+    document.getElementById('attempt').onchange = run_check;
+    document.getElementById('attempt').onkeyup = run_check;
+    run_check();
+}
+
+// When the requesting web page loads, initialize things
+window.onload = init_page;
---
+++ docs/labs/lab_runner.js
@@ -0,0 +1,111 @@
+// lab_runner - run labs
+
+// NOTE: THIS IS NOT BEING USED AND MAY BE DELETED SOON.
+// IT IS INCLUDED ONLY TO SHOW AN ALTERNATIVE APPROACH.
+
+// This is an early attempt at simulation of Express and express-validator.
+// This turned out to be much harder than expected, so we aren't using
+// this code, and it may get dropped completely.
+
+// We simulate *just* enough of express and express-validator to run labs
+// entirely locally on the client. This way, (1) learners don't need to
+// install *anything*, and (2) we don't have big server bills or security
+// issues from trying to run arbitrary user-provided code on our servers.
+
+const App = class {
+    routes = [];
+    constructor() {};
+    // See https://expressjs.com/en/guide/routing.html
+    get(path, ...callees) {
+        // TODO: Record functions. "Callees" will be recorded as an array.
+        this.routes.push('get', path, callees);
+        alert("You are in app.get() with path " + path + `and routes is now ${this.routes}`);
+    }
+}
+
+const app = new App();
+
+const Request = class {
+    constructor() {}
+    send(output) {this.outputcode = output; return this;}
+    status(code) {this.httpcode = code; return this;}
+};
+let req = new Request();
+
+// TODO
+
+// Example https://reflectoring.io/express-middleware/
+// const requireJsonContent = (request, response, next) => {
+//   if (request.headers['content-type'] !== 'application/json') {
+//       response.status(400).send('Server requires application/json')
+//   } else {
+//     next()
+//   }
+// }
+// function middlewareFunction(request, response, next){
+//  ...
+//  next()
+//}
+
+const regex_int = /^(?:[-+]?(?:0|[1-9][0-9]*))$/;
+
+const ExpressValidator = class {
+    name; // Field to validate
+    constructor(name) {this.name = name;};
+    isInt() {
+        return (req, res) => {
+            // TODO: min and max
+            return regex_int.test(req.parameters[name]);
+	}
+    };
+}
+
+// Express-validator's "query"
+const query = (name) => {
+    return new ExpressValidator(name);
+};
+
+function isInt() {};
+function validationResult(req) {return {}}; // Returns errors
+function matchedData(req) {return {}}; // Returns matchedData
+
+// In the real system:
+// query('id') = "async (req, _res, next) => {
+//         try {
+//             await runner.run(req);
+//             next();
+//         }
+//         catch (e) {
+//             next(e);
+//         }
+//     }"
+// typeof query('id') is string
+
+
+
+
+function run_lab() {
+    code = document.getElementById('code').value;
+    // NOTE: We *eval* the string. Evaluating arbitrary strings is
+    // usually insecure and dangerous. In this case, we know that the string
+    // is provided by the user and run on the same user's browser, so THIS
+    // use is okay. At worst, users are attacking themselves.
+    eval(code);
+}
+
+function init_page() {
+    document.getElementById('lab').onsubmit = run_lab;
+}
+
+// When the requesting web page loads, initialize things
+window.onload = init_page;
+
+// INLINE TEST
+app.get('/invoices', query('id').isInt({min: 1, max: 999999}), (req, res) => {
+//   const result = validationResult(req);
+//   if (result.isEmpty()) { // No errors
+//     const data = matchedData(req);
+//     return res.send(`You requested invoice id ${data.id}!`);
+//   }
+//   res.status(422).send("Invalid input");
+})
