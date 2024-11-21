438d4adf7f3ad0946ed90374b980e9ab4a195233
Hubert Daniszewski <61824500+s19110@users.noreply.github.com>
2024-11-21 12:12:01 +0100

    Update docs/Secure-Coding-Guide-for-Python/CWE-707/CWE-175/example03.py
    Co-authored-by: myteron <myteron@gmail.com>
    Signed-off-by: Hubert Daniszewski <61824500+s19110@users.noreply.github.com>

--- docs/Secure-Coding-Guide-for-Python/CWE-707/CWE-175/example03.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-707/CWE-175/example03.py
@@ -2,6 +2,18 @@
 # SPDX-License-Identifier: MIT
 """ Non-compliant Code Example """
 import locale
+ 
+def compare_number(number):
+    input_number = locale.atof(input(f"Enter a number {number}: "))
+    print(f"Locale is {locale.getlocale()}, you entered {input_number}.")
+    print(f"Does the number {number} match {input_number}? {number == input_number}")
+
+locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
+compare_number(12.345)
+
+# SPDX-License-Identifier: MIT
+""" Non-compliant Code Example """
+import locale
 ORIGINAL_NUMBER = 12.345  # This will read as 12,345 in German
  
 def compare_number(number):
