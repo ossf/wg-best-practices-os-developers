437708ff22ef3cd8f420894b1f1839bdba1df7bf
Hubert Daniszewski <61824500+s19110@users.noreply.github.com>
2024-11-28 16:43:00 +0100

    pySCG: Adding documentation to CWE-175 as part of #531 (#687)
    * Adding documentation to CWE-175 as part of #531
    
    Co-authored-by: BartyBoi1128 
    Co-authored-by: myteron <myteron@gmail.com>
    ---------
    
    Signed-off-by: edanhub <hubert.daniszewski@ericsson.com>
    Signed-off-by: Hubert Daniszewski <61824500+s19110@users.noreply.github.com>
    Co-authored-by: myteron <myteron@gmail.com>
    Co-authored-by: BartyBoi1128 <58297160+BartyBoi1128@users.noreply.github.com>

---
+++ docs/Secure-Coding-Guide-for-Python/CWE-707/CWE-175/compliant01.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+""" Compliant Code Example """
+import datetime
+import locale
+
+dt = datetime.datetime(2022, 3, 9, 12, 55, 35, 000000)
+
+CURRENT_LOCALE = 'en_IE.utf8'
+OTHER_LOCALE = 'uk_UA.utf8'
+
+#####################
+# Trying to exploit above code example
+#####################
+
+locale.setlocale(locale.LC_ALL, CURRENT_LOCALE)
+# Month is 'March'
+currmonth = dt.month
+locale.setlocale(locale.LC_ALL, OTHER_LOCALE)
+# Month is 'березень', i.e. berezen’
+othermonth = dt.month
+
+if currmonth == othermonth:
+    print("Locale-independent months are equal")
+else:
+    print("Locale-independent months are not equal")
---
+++ docs/Secure-Coding-Guide-for-Python/CWE-707/CWE-175/example02.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+""" Code Example """
+import locale
+CURRENT_LOCALE = 'en_IE.utf8'
+locale.setlocale(locale.LC_ALL, CURRENT_LOCALE)
