d108ea321941ac85d58cc7f838a2b0c63a3638f7
BartyBoi1128 <58297160+BartyBoi1128@users.noreply.github.com>
2024-12-11 11:18:36 +0000

    Update compliant01.py
    Updated the blank spaces.
    
    Signed-off-by: BartyBoi1128 <58297160+BartyBoi1128@users.noreply.github.com>

--- docs/Secure-Coding-Guide-for-Python/CWE-693/CWE-330/compliant01.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-693/CWE-330/compliant01.py
@@ -2,13 +2,13 @@
 # SPDX-License-Identifier: MIT
 """ Compliant Code Example """
 import secrets
- 
- 
+
+
 def generate_web_token():
     """Better cryptographic number generator"""
     return secrets.token_urlsafe()
- 
- 
+
+
 #####################
 # attempting to exploit above code example
 #####################
