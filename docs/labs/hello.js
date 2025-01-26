2883248c6e6687ddb4f057e7f50ec0431714a89c
David A. Wheeler <dwheeler@dwheeler.com>
2025-01-26 12:13:53 -0500

    Simplify hello.js
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- docs/labs/hello.js
+++ docs/labs/hello.js
@@ -1,87 +1,66 @@
 info =
 {
-  "hints": [
+  hints: [
     {
-      "absent": String.raw`^ console \. log \(`,
-      "text": " Please use the form console.log(\"...\");",
-      "examples": [
-        [ "" ],
-        [ "foo" ]
+      absent: String.raw`^ console \. log \(`,
+      text: "Please use the form console.log(\"...\");",
+      examples: [ [ "" ], [ "foo" ]
       ]
     },
     {
-      "present": "Goodbye",
-      "text": "You need to change the text Goodbye to something else.",
-      "examples": [
-        [ "console.log(\"Goodbye.\");" ]
-      ]
+      present: "Goodbye",
+      text: "You need to change the text Goodbye to something else.",
+      examples: [ [ "console.log(\"Goodbye.\");" ] ]
     },
     {
-      "present": "hello",
-      "text": "Please capitalize Hello.",
-      "examples": [
-        [ "console.log(\"hello.\");" ]
-      ]
+      present: "hello",
+      text: "Please capitalize Hello.",
+      examples: [ [ "console.log(\"hello.\");" ] ]
     },
     {
-      "present": "World",
-      "text": "Please lowercase world.",
-      "examples": [
-        [ "console.log(\"Hello, World!\");" ]
-      ]
+      present: "World",
+      text: "Please lowercase world.",
+      examples: [ [ "console.log(\"Hello, World!\");" ] ]
     },
     {
-      "present": "Hello[^,]",
-      "text": "Put a comma immediately after Hello.",
-      "examples": [
-        [ "console.log(\"Hello world.\");" ]
-      ]
+      present: "Hello[^,]",
+      text: "Put a comma immediately after Hello.",
+      examples: [ [ "console.log(\"Hello world.\");" ] ]
     },
     {
-      "present": "Hello",
-      "absent": "[Ww]orld",
-      "text": "There's a Hello, but you need to also mention the world.",
-      "examples": [
-        [ "console.log(\"Hello, \");" ]
-      ]
+      present: "Hello",
+      absent: "[Ww]orld",
+      text: "There's a Hello, but you need to also mention the world.",
+      examples: [ [ "console.log(\"Hello, \");" ] ]
     },
     {
-      "present": String.raw`world[^\!]`,
-      "text": "Put an exclamation point immediately after world.",
-      "examples": [
-        [ "console.log(\"Hello, world.\");" ]
-      ]
+      present: String.raw`world[^\!]`,
+      text: "Put an exclamation point immediately after world.",
+      examples: [ [ "console.log(\"Hello, world.\");" ] ]
     },
     {
-      "present": String.raw`Hello,\s*world!`,
-      "absent": String.raw`Hello,\x20world!`,
-      "text": "You need exactly one space between 'Hello,' and 'world!'",
-      "examples": [
-        [ "console.log(\"Hello,     world!\");" ]
-      ]
+      present: String.raw`Hello,\s*world!`,
+      absent: String.raw`Hello,\x20world!`,
+      text: "You need exactly one space between 'Hello,' and 'world!'",
+      examples: [ [ "console.log(\"Hello,     world!\");" ] ]
     },
     {
-      "present": String.raw`^ console \. log \( Hello`,
-      "text": "You must quote constant strings using \", ', or `",
-      "examples": [
-        [ "console.log(Hello, world" ],
-        [ "console.log( Hello, world" ]
-      ]
+      present: String.raw`^ console \. log \( Hello`,
+      text: "You must quote constant strings using \", ', or `",
+      examples: [ [ "console.log(Hello, world" ], [ "console.log( Hello, world" ] ]
     },
     {
-      "absent": String.raw` ; $`,
-      "text": "Please end this statement with a semicolon. JavaScript does not require a semicolon in this case, but usually when modifying source code you should follow the style of the current code.\n",
-      "examples": [
-        [ "  console.log(\"Hello, world!\")  " ]
-      ]
+      absent: String.raw` ; $`,
+      text: "Please end this statement with a semicolon. JavaScript does not require a semicolon in this case, but usually when modifying source code you should follow the style of the current code.\n",
+      examples: [ [ "  console.log(\"Hello, world!\")  " ] ]
     }
   ],
-  "successes": [
+  successes: [
     [ " console . log( \"Hello, world!\" ) ; " ],
     [ " console . log( 'Hello, world!' ) ; " ],
     [ " console . log( `Hello, world!` ) ; " ]
   ],
-  "failures": [
+  failures: [
     [ " console . log( Hello, world! ) ; " ],
     [ " console . log(\"hello, world!\") ; " ]
   ],
@@ -99,6 +78,5 @@ info =
       String.raw`\s* foo \s+ bar \\string\\ \s*`,
       String.raw`\s*foo\s+bar\s*\\string\\\s*`
     ]
-
   ]
 };
