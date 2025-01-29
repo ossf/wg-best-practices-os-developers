34f3a3097566cb9c0a028fccef58bda363022106
David A. Wheeler <dwheeler@dwheeler.com>
2025-01-29 15:09:15 -0500

    Improve assert.js
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- docs/labs/assert.js
+++ docs/labs/assert.js
@@ -2,18 +2,17 @@ info =
 {
   hints: [
     {
-      present: "assert\n",
+      present: "assert",
       text: "The whole point of this exercise is to NOT use `assert` as a way to validate input from untrusted users.",
       "examples": [
         [ "assert !bindingResult.hasErrors();\n" ]
       ]
     },
     {
-      absent: String.raw`^\s*if
-`,
+      absent: String.raw`^\s* if `,
       text: "Begin with `if` so you can return a result if there are errors.",
       "examples": [
-        [ "return \"form\";\n" ]
+        [ "return \"form\";" ]
       ]
     },
     {
@@ -25,29 +24,25 @@ info =
       text: "Java is case-sensitive. Use `hasErrors`, not `haserrors` nor `HasErrors`."
     },
     {
-      present: String.raw`^\s*if\s*[^\(\s]
-`,
+      present: String.raw`^\s*if\s*[^\(\s]`,
       text: "In Java, after the keyword `if` you must have an open left parenthesis. Conventionally there is one space between the `if` keyword and the open left parenthesis.",
       "examples": [
-        [ "if bindingResult.hasErrors\n" ]
+        [ "if bindingResult.hasErrors" ]
       ]
     },
     {
-      present: String.raw`^\s*if\s*\(\s*\!binding
-`,
+      present: String.raw`^\s*if\s*\(\s*\!binding`,
       text: "You have an extraneous `!` (not operator). Use the expression if (bindingResult.hasErrors()) ...",
       "examples": [
-        [ "if (!bindingResult.hasErrors())\n" ]
+        [ "if (!bindingResult.hasErrors())" ]
       ]
     },
     {
-      absent: String.raw`^ if \( bindingResult \. hasErrors \( \) \)
-`,
+      absent: String.raw`^ if \( bindingResult \. hasErrors \( \) \) `,
       text: "Begin the answer with the text `if (bindingResult.hasErrors())` so that a statement will be executed if that condition is true."
     },
     {
-      present: String.raw`if \( bindingResult \. hasErrors \( \) \) [^\{\s]
-`,
+      present: String.raw`if \( bindingResult \. hasErrors \( \) \) [^\{\s] `,
       text: "Follow the conditional with an open brace, e.g., `if (bindingResult.hasErrors()) {...`."
     },
     {
@@ -56,27 +51,23 @@ info =
       text: "You need to use `return \"form\";` somewhere."
     },
     {
-      present: String.raw`return "form"
-`,
+      present: String.raw`return "form"`,
       absent: String.raw`return "form" ;`,
       text: "You need to use `;` (semicolon) after `return \"form\"` because in Java statements must be followed by a semicolon."
     },
     {
-      absent: String.raw`\} $
-`,
+      absent: String.raw`\} $`,
       text: "The answer needs to end with `}` (closing brace)."
-    }
+    },
   ],
   successes: [
-    [
-      "if ( bindingResult.hasErrors() ) {\n    return \"form\";\n}\n",
-      "if ( bindingResult . hasErrors ( ) ) { return \"form\" ; }\n"
-    ]
+    [ "if ( bindingResult.hasErrors() ) {\n    return \"form\";\n}\n" ],
+    [ "if ( bindingResult . hasErrors ( ) ) { return \"form\" ; }\n" ],
   ],
   failures: [
-    "if ( bindingResult . hasErrors ( ) ) { return \"form\" }\n",
-    "if ( ! bindingResult . hasErrors ( ) ) { return \"form\" ; }\n",
-    "if bindingResult . hasErrors ( ) { return \"form\" ; }\n",
-    "if ( bindingResult . hasErrors ) { return \"form\" ; }\n"
+    [ "if ( bindingResult . hasErrors ( ) ) { return \"form\" }\n" ],
+    [ "if ( ! bindingResult . hasErrors ( ) ) { return \"form\" ; }\n" ],
+    [ "if bindingResult . hasErrors ( ) { return \"form\" ; }\n" ],
+    [ "if ( bindingResult . hasErrors ) { return \"form\" ; }\n" ],
   ]
 }
