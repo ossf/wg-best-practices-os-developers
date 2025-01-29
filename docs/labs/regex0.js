79ae3e50d86b1d97ca648319d0104cc263d4c562
David A. Wheeler <dwheeler@dwheeler.com>
2025-01-29 11:39:13 -0500

    Regex0 no yaml (#734)
    * Remove YAML from lab input1
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    * regex0.html lab - remove YAML
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>
    
    ---------
    
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

---
+++ docs/labs/regex0.js
@@ -0,0 +1,57 @@
+info =
+{
+  hints: [
+    {
+      present: "^$",
+      text: "You need to enter a pattern."
+    },
+    {
+      present: String.raw`^\^`,
+      text: "We are looking for 'cat' anywhere, not just the beginning, in this exercise."
+    },
+    {
+      present: "C",
+      text: "Regexes are normally case-sensitive. Use a lowercase c.",
+      "examples": [
+        [ "C" ]
+      ]
+    },
+    {
+      absent: "c",
+      text: "If you are searching for \"cat\" you need to look for a \"c\"",
+      "examples": [
+        [ "x" ]
+      ]
+    },
+    {
+      absent: "cat",
+      text: "The pattern \"cat\" is needed to search for \"cat\"."
+    },
+    {
+      absent: "A",
+      "index": 1,
+      text: "You need to mention A."
+    },
+    {
+      absent: "B",
+      "index": 1,
+      text: "You need to mention B."
+    },
+    {
+      absent: String.raw`A(\+|A\*)`,
+      "index": 1,
+      text: "Use \"A+\" to indicate \"one or more A\". You could also write \"AA*\"."
+    },
+    {
+      absent: String.raw`B(\+|B\*)`,
+      "index": 1,
+      text: "Use \"B+\" to indicate \"one or more B\". You could also write \"BB*\"."
+    }
+  ],
+  preprocessing: [
+    [
+      "\\s*",
+      ""
+    ]
+  ]
+}
