dbf4053eea7117a681d881f49ca7a4865bb7576c
myteron <myteron@gmail.com>
2025-04-23 18:37:12 +0100

    pySCG 707 180 2 GitHub (#846)
    * pySCG adding CWE-180 as part of #531
    
    ---------
    
    Signed-off-by: Helge Wehder <helge.wehder@ericsson.com>
    Signed-off-by: myteron <myteron@gmail.com>
    Co-authored-by: Hubert Daniszewski <61824500+s19110@users.noreply.github.com>
    Co-authored-by: andrew-costello

--- docs/Secure-Coding-Guide-for-Python/CWE-707/CWE-180/example01.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-707/CWE-180/example01.py
@@ -3,8 +3,8 @@
 import unicodedata
 
 print("\N{FULL STOP}" * 10)
-print("." == unicodedata.normalize("NFC", "\u2024") == "\N{FULL STOP}" == "\u002E")
-print("." == unicodedata.normalize("NFD", "\u2024") == "\N{FULL STOP}" == "\u002E")
-print("." == unicodedata.normalize("NFKC", "\u2024") == "\N{FULL STOP}" == "\u002E")
-print("." == unicodedata.normalize("NFKD", "\u2024") == "\N{FULL STOP}" == "\u002E")
+print("." == unicodedata.normalize("NFC", "\u2024") == "\N{FULL STOP}" == "\u002e")
+print("." == unicodedata.normalize("NFD", "\u2024") == "\N{FULL STOP}" == "\u002e")
+print("." == unicodedata.normalize("NFKC", "\u2024") == "\N{FULL STOP}" == "\u002e")
+print("." == unicodedata.normalize("NFKD", "\u2024") == "\N{FULL STOP}" == "\u002e")
 print("\N{FULL STOP}" * 10)
