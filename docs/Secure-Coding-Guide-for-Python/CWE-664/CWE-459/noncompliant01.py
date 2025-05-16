72b127b3a476dd7c2bb80850b3a85b0c9ca9423a
ewlxdnx <dean.wiley@ericsson.com>
2025-05-16 15:05:46 +0100

    adding CWE-459
    Signed-off-by: ewlxdnx <dean.wiley@ericsson.com>

---
+++ docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-459/noncompliant01.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+""" Non-compliant Code Example """
+f = open("tempfile.txt", "w")
+f.write("temporary file created!")
+f.close()
