3d932d89ba55a2ccbece74f4c858bd97bf391b05
myteron <myteron@gmail.com>
2025-05-19 14:13:01 +0100

    pySCG bugfix for CWE-191 as per #835 (#838)
    * pySCG bugfix for CWE-191 as per #835
    
    ---------
    Co-authored-by: BartKaras1128 <moezarts@gmail.com>
    Signed-off-by: myteron <myteron@gmail.com>
    Co-authored-by: andrew-costello <costelloandrew.work@gmail.com>

--- docs/Secure-Coding-Guide-for-Python/CWE-682/CWE-191/compliant02.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-682/CWE-191/compliant02.py
@@ -1,23 +1,71 @@
 # SPDX-FileCopyrightText: OpenSSF project contributors
 # SPDX-License-Identifier: MIT
-""" Compliant Code Example """
+"""Compliant Code Example"""
 
-import time
+from datetime import datetime, timedelta
+import logging
+
+# Enabling verbose debugging
+# logging.basicConfig(level=logging.DEBUG)
 
 
-def get_time_in_future(hours_in_future):
-    """Gets the time n hours in the future"""
-    try:
-        currtime = list(time.localtime())
-        currtime[3] = currtime[3] + hours_in_future
-        if currtime[3] + hours_in_future > 24:
-            currtime[3] = currtime[3] - 24
-        return time.asctime(tuple(currtime)).split(" ")[3]
-    except OverflowError as _:
-        return "Number too large to set time in future " + str(hours_in_future)
+def get_datetime(currtime: datetime, hours: int):
+    """
+    Gets the time n hours in the future or past
+
+    Parameters:
+    currtime (datetime): A datetime object with the starting datetime.
+    hours (int): Hours going forward or backwards
+
+    Returns:
+    datetime: A datetime object
+    """
+    # TODO: input sanitation
+    # Calculate lower boundary, hours from year 1 to currtime:
+    timedelta_lowerbound = currtime - datetime(1, 1, 1)  # 1st Jan 0001
+    hours_min = timedelta_lowerbound.total_seconds() // 3600 * -1
+
+    # Calculate upper boundary, hours from year 9999 to currtime:
+    timedelta_upperbound = datetime(9999, 12, 31) - currtime
+    hours_max = timedelta_upperbound.total_seconds() // 3600
+
+    # TODO: proper secure logging
+    logging.debug(
+        "hours_max=%s hours_min=%s hours=%s",
+        hours_max,
+        hours_min,
+        hours,
+    )
+    if (hours > hours_max) or (hours < hours_min):
+        raise ValueError("hours out of range")
+
+    return currtime + timedelta(hours=hours)
 
 
 #####################
 # attempting to exploit above code example
 #####################
-print(get_time_in_future(23**74))
+datetime.fromtimestamp(0)
+currtime = datetime.fromtimestamp(1)  # 1st Jan 1970
+
+# OK values are expected to work
+# NOK values trigger OverflowErrors in libpython written in C
+hours_list = [
+    0,  # OK
+    1,  # OK
+    70389526,  # OK
+    70389527,  # NOK
+    51539700001,  # NOK
+    24000000001,  # NOK
+    -1,  # OK
+    -17259889,  # OK
+    -17259890,  # NOK
+    -23999999999,  # NOK
+    -51539699999,  # NOK
+]
+for hours in hours_list:
+    try:
+        result = get_datetime(currtime, hours)
+        print(f"{hours} OK, datetime='{result}'")
+    except Exception as exception:
+        print(f"{hours} {repr(exception)}")
