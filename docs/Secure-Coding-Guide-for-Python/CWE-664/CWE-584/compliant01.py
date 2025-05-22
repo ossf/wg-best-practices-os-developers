4f12fd0c761a72deeeeb02e3c64f06e309e76320
Andrew Costello <andrew.costello@ericsson.com>
2025-05-22 12:29:54 +0100

    CWE-584
    Signed-off-by: Andrew Costello <andrew.costello@ericsson.com>

---
+++ docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-584/compliant01.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+ 
+"""Compliant Code Example"""
+ 
+ 
+def do_logic():
+    try:
+        raise Exception
+    finally:
+        print("logic done")
+    # return statement goes here
+    # when exception is raised conditionally
+    return True
+ 
+ 
+#####################
+# exploiting above code example
+#####################
+do_logic()
---
+++ docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-584/compliant02.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+ 
+def do_logic():
+    try:
+        raise Exception
+    finally:
+        c = 0
+        while c < 5:
+            print(f"c is {c}")
+            c += 1
+            if c == 3:
+                break
+    # return statement goes here
+    # when exception is raised conditionally
+    return True
+ 
+ 
+#####################
+# exploiting above code example
+#####################
+do_logic()
