ce1a980ffa56fd596c4e49cbb7864ca71662abcd
Muuhh IKEDA <munehiro.ikeda@cybertrust.co.jp>
2025-05-22 16:07:06 +0900

    Japanese translation of conversion
    Japanese translaton for docs/labs/conversion.html and conversion.js
    
    Signed-off-by: Muuhh IKEDA <munehiro.ikeda@cybertrust.co.jp>

--- docs/labs/conversion.js
+++ docs/labs/conversion.js
@@ -7,10 +7,12 @@ info =
     {
       absent: "unsigned",
       text: "The type defined for queue_count should exactly match the return type of get_queue.",
+      text_ja: "queue_count の型定義は get_queue の戻り値の型と正確に一致している必要があります。",
     },
     {
       present: String.raw`unsigned\s+queue_count`,
       text: "The declared return type of get_queue is `unsigned int`; you should match it exactly instead of using a synonym like `unsigned`.",
+      text_ja: "get_queue のの戻り値の型宣言は `unsigned int` です。`unsigned` のようなシノニム（同義語）ではなく、これに正確に一致させましょう。",
     },
   ],
   expected: [
