08a525fb970db6ba019249bd40163fded2ee96b7
Muuhh IKEDA <munehiro.ikeda@cybertrust.co.jp>
2025-05-26 18:52:02 +0900

    Japanese translation of free
    Japanese translaton for docs/labs/free.html and free.js
    
    Signed-off-by: Muuhh IKEDA <munehiro.ikeda@cybertrust.co.jp>

--- docs/labs/free.js
+++ docs/labs/free.js
@@ -7,6 +7,7 @@ info =
     {
       present: String.raw`\s*free[^;]*; asprintf`,
       text: "Do not free the input first, you need to use it.",
+      text_ja: "入力を最初に free しないでください。それは使う必要があります。",
       examples: [
         [
           "free(s);\nasprintf(&result, \"pre_%s_post\", s);"
@@ -17,6 +18,7 @@ info =
       present: String.raw`\s* asprintf \(`,
       absent: "free",
       text: "This fails to free the memory, likely leading to a missing release.",
+      text_ja: "メモリを解放し忘れています。",
       examples: [
         [
           "asprintf(&result, \"\"pre_%s_post\"\", s);"
@@ -26,10 +28,12 @@ info =
     {
       absent: "return",
       text: "This fails to return the result.",
+      text_ja: "結果を返し忘れています。",
     },
     {
       absent: String.raw`\s* [^;]+;[^;]+;[^;]+; \s*`,
       text: "There should be 3 statements, each terminated with a semicolon.",
+      text_ja: "それぞれがセミコロンで終わる３つのステートメントが必要です。",
       examples: [
         [
           "asprintf(&result, \"pre_%s_post\", s);\nfree(s);\nreturn result"
@@ -39,6 +43,7 @@ info =
     {
       present: String.raw`\s* return result ; free \s*`,
       text: "Do not do anything after the return, it will not execute.",
+      text_ja: "return のあとは何もしないでください。それは実行されません。",
       examples: [
         [
           "asprintf(&result, \"pre_%s_post\", s);\nreturn result;\nfree(s);"
