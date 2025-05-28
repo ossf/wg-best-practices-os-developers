73385047eb58fac6d6e804324bf0cdc47a5f5f17
andrew-costello <costelloandrew.work@gmail.com>
2025-05-28 08:51:07 +0100

    Update docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-584/noncompliant01.py
    Co-authored-by: Bartlomiej Karas <moezarts@gmail.com>
    Signed-off-by: andrew-costello <costelloandrew.work@gmail.com>

--- docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-584/noncompliant01.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-584/noncompliant01.py
@@ -1,17 +1,17 @@
 # SPDX-FileCopyrightText: OpenSSF project contributors
 # SPDX-License-Identifier: MIT
- 
+
 """Non-compliant Code Example"""
- 
- 
+
+
 def do_logic():
     try:
         raise Exception
     finally:
         print("logic done")
         return True
- 
- 
+
+
 #####################
 # exploiting above code example
 #####################
