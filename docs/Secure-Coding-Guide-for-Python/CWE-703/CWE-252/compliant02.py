6310c10634792cff063489e47bb5cd5c02c75fdd
Hubert Daniszewski <61824500+s19110@users.noreply.github.com>
2025-06-30 16:53:42 +0200

    Update compliant02.py
    Signed-off-by: Hubert Daniszewski <61824500+s19110@users.noreply.github.com>

--- docs/Secure-Coding-Guide-for-Python/CWE-703/CWE-252/compliant02.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-703/CWE-252/compliant02.py
@@ -7,8 +7,7 @@ def find_in_string(full_string, sub_string):
     """Function that searches for a sub-string in a given string"""
     index = full_string.find(sub_string)
     if index >= 0:
-        print(f"""Sub-string '{sub_string}' appears for the first time in
-              '{full_string}' at index {index}: '{full_string[index:]}'""")
+        print(f"Sub-string '{sub_string}' appears in '{full_string}' at index {index}'")
     else:
         print(f"There is no '{sub_string}' in '{full_string}'")
 
