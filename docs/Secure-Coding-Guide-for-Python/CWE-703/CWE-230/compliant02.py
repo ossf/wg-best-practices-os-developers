3a4f6273b2f57f40c22b3613283db188acdb492c
ewlxdnx <dean.wiley@ericsson.com>
2025-07-22 12:02:00 +0100

    CWE-230 Improper handling of missing values
    Signed-off-by: ewlxdnx <dean.wiley@ericsson.com>

---
+++ docs/Secure-Coding-Guide-for-Python/CWE-703/CWE-230/compliant02.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+""" Non-compliant Code Example """
+import math
+ 
+ 
+def balance_is_positive(value: str) -> bool:
+    """Returns True if there is still enough value for a transaction"""
+    _value = float(value)
+    if math.isnan(_value) or _value is None:
+        raise ValueError("Expected a float")
+    if _value <= 0:
+        return False
+    else:
+        return True
+ 
+ 
+#####################
+# attempting to exploit above code example
+#####################
+print(balance_is_positive("0.01"))
+print(balance_is_positive("0.001"))
+print(balance_is_positive("NaN"))
