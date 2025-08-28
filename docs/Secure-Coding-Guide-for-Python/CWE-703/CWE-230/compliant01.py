6d5bf6ce13c9b40304afbfa6e1c3f4e8c1d24116
dwiley258 <dean.wiley@ericsson.com>
2025-08-28 15:17:50 +0100

    Python Secure Coding Guide: CWE-230 (#967)
    Signed-off-by: ewlxdnx <dean.wiley@ericsson.com>

--- docs/Secure-Coding-Guide-for-Python/CWE-703/CWE-230/compliant01.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-703/CWE-230/compliant01.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: OpenSSF project contributors
 # SPDX-License-Identifier: MIT
-""" Non-compliant Code Example """
+""" Compliant Code Example """
 
 from decimal import ROUND_DOWN, Decimal
 
