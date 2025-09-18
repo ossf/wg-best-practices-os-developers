ddd9a3d7e4f0c3ee3dd56582a11944f53f259afb
Helge Wehder <helge.wehder@ericsson.com>
2025-09-18 12:02:53 +0100

    also added comment changes to the complian01.py code
    Signed-off-by: Helge Wehder <helge.wehder@ericsson.com>

--- docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-460/compliant01.py
+++ docs/Secure-Coding-Guide-for-Python/CWE-664/CWE-460/compliant01.py
@@ -11,9 +11,8 @@ def perform_critical_operation():
     with lock:
         # the lock has been acquired using the 'with' statement and will be released when the block exits; even if an exception occurs
         print("Lock acquired, performing critical operation...")
-        # raising an exception
         raise ValueError("Something went wrong!")
-        # This line will not be reached because of the exception above,
+        # This line will not be reached because of the exception above
     print("Lock released.")
 
 
