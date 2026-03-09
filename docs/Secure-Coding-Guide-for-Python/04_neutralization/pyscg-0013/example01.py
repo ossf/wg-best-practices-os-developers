73a5570583b4ef2a5da2c396dc56df261b80b34b
Bartlomiej Karas <moezarts@gmail.com>
2026-03-09 16:18:33 +0000

    Create example01.py
    Signed-off-by: Bartlomiej Karas <moezarts@gmail.com>

---
+++ docs/Secure-Coding-Guide-for-Python/04_neutralization/pyscg-0013/example01.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+"""Example Code Example"""
+
+import sysconfig
+import sys
+print("sysconfig.get_paths().values())", end=" ")
+print(sysconfig.get_paths().values())
+print("")
+print("print(sys.path)", end=" ")
+print(print(sys.path))
