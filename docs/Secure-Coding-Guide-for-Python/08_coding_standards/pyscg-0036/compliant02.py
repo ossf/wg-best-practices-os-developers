b7967858e437d2fd73decec7cbce6dd0a2888dcf
Hubert Daniszewski <61824500+s19110@users.noreply.github.com>
2026-04-12 13:35:41 +0200

    Update docs/Secure-Coding-Guide-for-Python/08_coding_standards/pyscg-0036/compliant02.py
    Co-authored-by: myteron <myteron@gmail.com>
    Signed-off-by: Hubert Daniszewski <61824500+s19110@users.noreply.github.com>

--- docs/Secure-Coding-Guide-for-Python/08_coding_standards/pyscg-0036/compliant02.py
+++ docs/Secure-Coding-Guide-for-Python/08_coding_standards/pyscg-0036/compliant02.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: OpenSSF project contributors
 # SPDX-License-Identifier: MIT
-""" Non-compliant Code Example """
+""" Compliant Code Example """
 
 
 def wrap_in_quotes(full_string, sub_string):
