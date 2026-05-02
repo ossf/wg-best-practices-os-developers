3fd1a96bc43f5f34130eb34a899cc90a2e354e0e
s19110 <hubertdan24@gmail.com>
2026-05-02 12:26:01 +0200

    Added compliant01.py
    Signed-off-by: s19110 <hubertdan24@gmail.com>

---
+++ docs/Secure-Coding-Guide-for-Python/04_neutralization/pyscg-0009/compliant01.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: OpenSSF project contributors
+# SPDX-License-Identifier: MIT
+""" Compliant Code Example """
+import os
+from pathlib import Path
+
+class FileOperations:
+    """Helper class for file system operations"""
+
+    def list_dir(self, dirname: str):
+        """List the contents of a directory"""
+        path = Path(dirname)
+        # TODO: input sanitation
+        # TODO: Add secure logging
+        if path.is_dir():
+            for item in path.iterdir():
+                print(item)
+
+
+#####################
+# Trying to exploit above code example
+#####################
+if "nt" in os.name:
+    FileOperations().list_dir("temp")
+if "posix" in os.name:
+    FileOperations().list_dir("/etc/shadow; head -1 /etc/passwd")
